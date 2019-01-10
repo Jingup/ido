35fa7a4318a76ff749b0ae5e1bcf38dd022210b0
shark <shark@example.com>
2019-01-10 14:44:46 +0800

    1.10

--- bakup.sh
+++ bakup.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-mysqldump -uroot -p'***' crane > /data/backup/db/crane.sql
+#rpm -q expect > /dev/null
+#[ $? -ne 0 ] && yum -y install expect
+#rpm -q ftp > /dev/null
+#[ $? -ne 0 ] && yum -y install ftp
+mysqldump -uroot -p'***' test > /data/backup/db/test.sql
 sleep 1
 cd /data/backup/
 D=`date +%Y%m%d`
 DM=`date -d "a month ago" +%Y%m%d`
-ls crane-$DM.tar.gz >&2 /dev/null
-[ $? -eq 0 ] && rm -f crane-$DM.tar.gz
-tar -czf crane-$D.tar.gz crane/ db/
+ls test-$DM.tar.gz > /dev/null 2>&1
+[ $? -eq 0 ] && rm -f test-$DM.tar.gz
+tar -czf test-$D.tar.gz test/ db/
 expect <<EOF
 set timeout -1
 spawn ftp *.*.*.* *
 expect "Name" { send "rsync\r" }
 expect "Password:" { send "***\r" }
-expect "ftp>" { send "put crane-$D.tar.gz\r" }
+expect "ftp>" { send "put test-$D.tar.gz\r" }
 expect "ftp>" { send "exit\r" }
 EOF
+
